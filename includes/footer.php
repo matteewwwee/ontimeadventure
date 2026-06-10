76e2c3d8c0c3c0ebbaf68fc7a7adabfdd149df4f
matt10112002 <RahmatRamadhan349@gmail.com>
2026-06-11 06:04:06 +0800

    Simplify footer text

--- includes/footer.php
+++ includes/footer.php
@@ -19,14 +19,7 @@ $yearDisplay = ($currentYear > $startYear) ? "{$startYear} - {$currentYear}" : "
     <!-- Footer Start -->
     <footer class="footer mt-auto py-3 bg-white text-center">
         <div class="container">
-            <span class="text-muted"> Copyright © <span id="year"><?= $yearDisplay ?></span> <a
-                    href="javascript:void(0);" class="text-dark fw-semibold">On Time Adventure</a>.
-                Designed with <span class="bi bi-heart-fill text-danger"></span> by <a href="javascript:void(0);">
-                    <span class="fw-semibold text-primary text-decoration-underline">Your Team</span>
-                </a> All
-                rights
-                reserved
-            </span>
+            <span class="text-muted">Copyright &copy; 2026 OnTimeAdventure</span>
         </div>
     </footer>
     <!-- Footer End -->
